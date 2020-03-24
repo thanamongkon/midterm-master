b42bf4c9fb6032d6f91e35d012857432e92673a9
thanamongkon <59908382+thanamongkon@users.noreply.github.com>
2020-03-24 09:27:45 +0700

    commit2

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <HEAD><TITLE> My Homepage </TITLE></HEAD>
 <BODY BGCOLOR=#FFFFFF>
 <H1>
-<?php echo "รหัสนิสิต-62310xxx"; ?><br>
-<?php echo "ชื่อ-สกุล นาย........."; ?><br>
+<?php echo "รหัสนิสิต-62310022"; ?><br>
+<?php echo "ชื่อ-สกุล นายธนมงคล เสนะวิทูรย์กิจ"; ?><br>
 <iframe width="500" height="500"
 src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
 </iframe>
