a89f5e2ef733ef370722e6e8ce70210d47f4ee1d
thanamongkon <59908382+thanamongkon@users.noreply.github.com>
2020-03-24 09:25:05 +0700

    commit1

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310xxx"; ?><br>
+<?php echo "ชื่อ-สกุล นาย........."; ?><br>
+<iframe width="500" height="500"
+src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
+</iframe>
+</BODY>
+</HTML>
